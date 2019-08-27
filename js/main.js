7ef3ec1920457be47595b91e89f3beda66c1bb68
Martha VanderMeulen <48190780+marthavander@users.noreply.github.com>
2019-08-27 14:34:27 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+ScrollReveal().reveal('.cs-top-image', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-container-summary', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.callout-1', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-persona-pic', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.competitive-logo', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.callout-2', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-chart1', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-chart2', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.user-flows-image1', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-sketch-wires1', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.logo-ideas-row', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-hl-branding', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-center', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-hi-fi1', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-hi-fi2', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-hi-fi3', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-full-image2', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.callout-3', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.competitive-logo2', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-rt-prototypes', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-rt-prototypes-mobile', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-rt-visualdesign', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-rt-visualdesign-mobile', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+ScrollReveal().reveal('.cs-ar-wires', {delay: 400, distance: '20px', easing: 'cubic-bezier(0.5, 1, 1, 1)'});
+
+
+var slideLeft = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null,
+    delay: '400',
+    easing: 'cubic-bezier(0.5, 1, 1, 1)'
+};
+
+ScrollReveal().reveal('.hello', slideLeft);
+ScrollReveal().reveal('.project2', slideLeft);
+
+
+var slideRight = {
+    distance: '150%',
+    origin: 'right',
+    opacity: null,
+    delay: '400',
+    easing: 'cubic-bezier(0.5, 1, 1, 1)'
+};
+
+ScrollReveal().reveal('.project1', slideRight);
+ScrollReveal().reveal('.project3', slideRight);
